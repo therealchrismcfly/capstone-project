933ab0fd2873e279b639d94cf6f6503672164636
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-08-15 11:37:17 +0200

    add styled card and headline

---
+++ src/components/Card Headline/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const StyledHeadline = styled.h2`
+	font-size: 26px;
+`;
+export default StyledHeadline;
---
+++ src/components/Card/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledCard = styled.section`
+	width: 100px;
+	height: 300px;
+	background-color: lightblue;
+`;
+
+export default StyledCard;
