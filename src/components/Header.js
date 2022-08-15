43dc649175b58b30f3e03a1564b57627648db671
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-08-15 16:34:05 +0200

    delete useless dummy code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,7 @@
-import Link from 'next/link';
-
 export default function Header() {
 	return (
 		<header>
-			<nav>
-				<Link href="/">Home</Link>
-				<Link href="/about">About me</Link>
-			</nav>
+			<nav></nav>
 		</header>
 	);
 }
