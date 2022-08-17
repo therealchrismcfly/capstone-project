19a1078f9bb978b213cebb596e73a8cadfbec49c
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-08-17 11:26:16 +0200

    add cards with useStore

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,18 +1,28 @@
 import {exercises} from '../../../backend/db';
+import useStore from '../../hooks/useStore';
 import StyledCard from '../Card/styled';
-import useStore from '../hooks/useStore';
 
 import StyledCardlist from './styled';
 
 function Cardlist() {
 	const cards = useStore(state => state.cards);
 	const addCard = useStore(state => state.addCard);
+	const deleteCard = useStore(state => state.deleteCard);
 
 	return (
 		<>
 			<p>Ausgewählte Übungen</p>
 			{cards.map(card => {
-				return <StyledCard key={card.id}>{card.name}</StyledCard>;
+				return (
+					<StyledCard
+						key={card.id}
+						onClick={() => {
+							deleteCard(card.id);
+						}}
+					>
+						{card.name}
+					</StyledCard>
+				);
 			})}
 			<p>Alle Übungen</p>
 			<StyledCardlist>
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,6 +3,15 @@ import create from 'zustand';
 
 const useStore = create(set => ({
 	cards: [],
+
+	deleteCard: id => {
+		set(state => {
+			return {
+				cards: state.cards.filter(card => card.id !== id),
+			};
+		});
+	},
+
 	addCard: name => {
 		set(state => {
 			return {
