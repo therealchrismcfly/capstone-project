fab02560e4d45f1e228df238929b80323754d195
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-08-17 12:00:32 +0200

    add calendar dummy button

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,6 +1,8 @@
 import {exercises} from '../../../backend/db';
 import useStore from '../../hooks/useStore';
+import StyledCalendarButton from '../Buttons/CalendarButton/styled';
 import StyledCard from '../Card/styled';
+import StyledHeadline from '../CardHeadline/styled';
 
 import StyledCardlist from './styled';
 
@@ -13,13 +15,15 @@ function CardlistIndex() {
 			<StyledCardlist>
 				{exercises.map(exercise => {
 					return (
-						<StyledCard
-							key={exercise._id}
-							onClick={() => {
-								addCard(exercise.name);
-							}}
-						>
-							{exercise.name}
+						<StyledCard key={exercise._id}>
+							<StyledCalendarButton
+								onClick={() => {
+									addCard(exercise.name);
+								}}
+							>
+								{'hello'}
+							</StyledCalendarButton>
+							<StyledHeadline>{exercise.name}</StyledHeadline>
 						</StyledCard>
 					);
 				})}
