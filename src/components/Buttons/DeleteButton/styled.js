fc93d05e531e3ab9e229f8a43288534f62102a34
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-08-18 11:12:11 +0200

    add delete button

---
+++ src/components/Buttons/DeleteButton/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledDeleteButton = styled.button`
+	margin-right: 10px;
+	padding: 10px;
+	border: 4px solid red;
+`;
+
+export default StyledDeleteButton;
