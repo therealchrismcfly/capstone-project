11ea8bd99d04c27874c76f1df03b11f8c683d551
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-09-01 12:02:07 +0200

    improve ux

--- src/components/Searchbar/styled.js
+++ src/components/Searchbar/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const StyledSearchBar = styled.form`
+const StyledSearchBar = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
