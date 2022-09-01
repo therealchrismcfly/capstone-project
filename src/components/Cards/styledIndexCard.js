0c7b0d69f6337bda161c0751546fc1fc1153f42b
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-09-01 15:06:45 +0200

    bug fix

--- src/components/Cards/styledIndexCard.js
+++ src/components/Cards/styledIndexCard.js
@@ -4,7 +4,7 @@ const StyledIndexCard = styled.li`
 	position: relative;
 	width: 400px;
 	height: 400px;
-	margin-bottom: 60px;
+	margin-bottom: 50px;
 	border-radius: 25px;
 	background-color: lightblue;
 	box-shadow: 0 4px 40px rgba(0, 0, 0, 0.25);
