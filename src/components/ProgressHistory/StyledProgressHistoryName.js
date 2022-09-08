54f275966ea721bd068e0e67c7cc1827b5d86fed
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-09-08 14:32:23 +0200

    fix progress bug

--- src/components/ProgressHistory/StyledProgressHistoryName.js
+++ src/components/ProgressHistory/StyledProgressHistoryName.js
@@ -4,6 +4,7 @@ const StyledProgressHistoryName = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	justify-content: center;
 	width: 375px;
 	background-color: var(--lavender);
 	box-shadow: 0 4px 40px rgba(0, 0, 0, 0.25);
