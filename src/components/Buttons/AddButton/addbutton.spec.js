56a8dbe14c2ac3988f08170ff0a309e4b287ea5d
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-09-10 15:30:05 +0200

    error testing

---
+++ src/components/Buttons/AddButton/addbutton.spec.js
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Button from './styled';
+
+describe('Button component', () => {
+	it('should allow clicks', async () => {
+		const handleClick = jest.fn();
+		const text = 'add';
+		render(<Button onClick={handleClick}>{text}</Button>);
+		const button = screen.getByText(text);
+		await userEvent.click(button);
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+});
